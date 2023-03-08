831c7b30339d3a9a397809e9f3683bc8852da3e2
hectorsalg <hectorsalg@ufpi.edu.br>
2023-03-08 15:21:51 -0300

    Criei ordenação selection na struct Notas

--- notas.c
+++ notas.c
@@ -17,9 +17,27 @@ Notas *inserirNotas(Notas *listaNotas){
     scanf("%f", new->nota);
     new->prox = NULL;
     new->ante = NULL;
-    
 
     if(!listaNotas) return new;
 
     return new->prox = listaNotas;
+}
+
+void *selection_sort(Notas *cabeca) {
+    Notas *aux, *aux2, *min;
+    
+    int aux3;
+    for (aux = cabeca; aux !=NULL; aux = aux->prox) {
+        min = aux;
+        for (aux2 = aux->prox; aux2 != NULL; aux2 = aux2->prox){
+            if (aux2->nota < min->nota) {
+                min = aux2;
+            }
+        }
+        if (aux != min) {
+            aux3 = aux->nota;
+            aux->nota = min->nota;
+            min->nota = aux3;
+        }
+    }
 }
--- notas.h
+++ notas.h
@@ -2,3 +2,4 @@ typedef struct notas Notas;
 
 Notas *iniciarNotas();
 Notas *inserirNotas(Notas *listaNotas);
+void selection_sort(Notas *cabeca);
