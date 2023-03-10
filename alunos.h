b40a29059b0f9bdd80986a8bff58fd4561dea32c
Willians-S-S <09willians@gmail.com>
2023-03-10 19:57:17 -0300

    ordenação deu certo, selectionSort do youtube

--- alunos.h
+++ alunos.h
@@ -4,6 +4,8 @@ Alunos *iniciar();
 Alunos *inserirAlunos(Alunos *listaDis);
 void mostrarAlunos(Alunos * listaAlun);
 
+void selecionSort(Alunos * listaAlun);
+
 void Insert_Sort(Alunos *listaAlun);
 
 void insertion_sort(char **lista, int n);
